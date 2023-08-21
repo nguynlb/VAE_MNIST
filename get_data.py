2169f7c96d86372daeb6672f7f6166d67d616648
Long Bảo <longbao.it@gmail.com>
2023-08-21 16:41:50 +0700

    Update get_data.py
    remove test dataset

--- get_data.py
+++ get_data.py
@@ -15,10 +15,9 @@ NUM_WORKERS = os.cpu_count()
 # DataLoader
 def create_data(data_dir: str,
                 train_transform: Compose,
-                test_transform: Compose,
                 batch_size: int = BATCH_SIZE,
                 num_workers: int = NUM_WORKERS) -> \
-        Tuple[DataLoader, DataLoader, List[str]]:
+        Tuple[DataLoader, List[str]]:
     """
        Pipeline handle data. Download MNIST dataset and create dataloader instance.
        :param data_dir: path to data directory, if not exits, create
@@ -33,17 +32,11 @@ def create_data(data_dir: str,
     # If true, skip downloading.
     if data_path_dir.is_dir():
         print("Data has been created. Skip downloading...")
-        train_dataset = MNIST(root=data_path_dir / "train",
+        train_dataset = MNIST(root=data_path_dir,
                               train=True,
                               transform=train_transform,
                               download=False,
                               target_transform=None)
-
-        test_dataset = MNIST(root=data_path_dir / "test",
-                             train=False,
-                             transform=test_transform,
-                             download=False,
-                             target_transform=None)
 
     else:
         data_path_dir.mkdir(parents=True, is_exists=True)
@@ -53,12 +46,6 @@ def create_data(data_dir: str,
                               transform=train_transform,
                               download=False,
                               target_transform=None)
-
-        test_dataset = MNIST(root=data_path_dir / "test",
-                             train=False,
-                             transform=test_transform,
-                             download=False,
-                             target_transform=None)
 
         print("Download successfully")
 
@@ -70,10 +57,5 @@ def create_data(data_dir: str,
                                   num_workers=num_workers,
                                   shuffle=True,
                                   pin_memory=True)
-    test_dataloader = DataLoader(dataset=test_dataset,
-                                 batch_size=batch_size,
-                                 num_workers=num_workers,
-                                 shuffle=False,
-                                 pin_memory=True)
 
-    return train_dataloader, test_dataloader, class_names
+    return train_dataloader, class_names
