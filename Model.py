df122f1742011355e397dbc73ae4b3d52c620f50
Long Bảo <114165590+LongBaoCoder2@users.noreply.github.com>
2023-08-31 11:08:54 +0700

    Model

--- Model.py
+++ Model.py
@@ -3,14 +3,32 @@ import torch
 import torch.nn as nn
 
 class VAE(nn.Module):
-    def encoder(self, in_dim: int, h_dim: int) -> Tuple[torch.Tensor, torch.Tensor]:
+    def encoder(self, x: torch.Tensor) -> Tuple[nn.Module, nn.Module]:
+        submodule = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features=28*28, out_features=256),
+            nn.ReLU()
+        )
+
+        mean_layer = nn.Linear(in_features=256, out_features=200)
+        std_layer = nn.Linear(in_features=256, out_features=self.h_dim)
+
+        _hidden = submodule(x)
+        return mean_layer(nn.Softmax(_hidden)), std_layer(nn.ReLu(_hidden))
+
+    def decoder(self, z: torch.Tensor) -> torch.Tensor:
+        model = nn.Sequential(
+            nn.Linear(in_features=self.h_dim, out_features=256),
+            nn.Linear(in_features=256, out_features=28*28)
+        )
+
+        return model(z).reshape((-1, 1, 28, 28))
+
+    def reparameter_trick(self, mean: torch.Tensor, std: torch.Tensor) -> torch.Tensor:
         pass
 
-    def decoder(self, in_dim: int, h_dim: int) -> torch.Tensor:
-        pass
+    def __init__(self, h_dim: int) -> None:
+        self.h_dim = h_dim
 
-    def __int__(self):
-        pass
 
-    def forward(self, x: torch.Tensor):
-        pass
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
